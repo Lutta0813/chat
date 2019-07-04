5669a1f76d72a591e5ca6046f1f2959bb362884c
LuTsungTa <steven82813@gmail.com>
2019-07-04 15:48:20 +0800

    upload chat.py

---
+++ chat.py
@@ -0,0 +1,34 @@
+# 讀取檔案
+def read_file(filename):
+	chatData = []
+	with open(filename, 'r') as f:
+		for line in f:
+			# print(line)
+			chatData.append(line)
+	return chatData
+# 改寫格式
+def chatFormat(chatData ,chater_name_1, chater_name_2):
+	refactorData = []
+	checkWhosTalking = ''
+	for line in chatData:
+		if chater_name_1 in line:
+			checkWhosTalking = chater_name_1
+			continue
+		elif chater_name_2 in line:
+			checkWhosTalking = chater_name_2
+			continue
+		else:
+			refactorData.append(checkWhosTalking + ': ' + line)
+	return refactorData
+
+# 寫入檔案
+def write_file(filename, refactorData):
+	with open(filename, 'w', encoding = 'utf-8') as f:
+		for line in refactorData:
+			f.write(line)
+
+
+
+chatData = read_file('input.txt')
+refactorData = chatFormat(chatData, 'Allen', 'Tom')
+write_file('output.txt', refactorData)
